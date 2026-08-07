8182b408afdbbd5fc51ad35e1cc37698ae329f5b
Kevin <nodgez@hotmail.com>
2026-08-07 10:49:02 +0100

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        var set = new HashSet<int>();
+
+        for(int i = 0; i < nums.Length;i++)
+        {
+            var n = nums[i];
+            var diff = target - n;
+
+            if(set.Contains(diff))
+            {
+                var j = Array.IndexOf(nums, diff);
+                return new int[2]{j, i};
+            }
+
+            set.Add(n);
+        }
+
+        return new int[0];
+    }
+}
